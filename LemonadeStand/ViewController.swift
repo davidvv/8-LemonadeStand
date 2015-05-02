ac04b22c0e875671fc3421af01978011663b0514
davidvv <idavidvv@gmail.com>
2015-05-02 17:40:51 +0200

    Buy works
    Buy buttons are working

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -23,6 +23,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var mixLemonLabel: UILabel!
     @IBOutlet weak var mixIceLabel: UILabel!
     
+    var haveCash = 10
+    var haveLemons = 1
+    var haveIce = 1
+    
+    var buyLemons = 0
+    var buyIce = 0
+    
+    //tengo que meter en algún sitio una función que calcule la acidez, siendo de 0-0,4 ácido, 0,4-0,6 medio y 0,6-1 diluido
+    var mixLemon = 0
+    var mixIce = 0
+    
     
     
     
@@ -32,6 +43,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        haveCashLabel.text = "$\(haveCash)"
+        haveLemonsLabel.text = "\(haveLemons) lemon"
+        haveIceLabel.text = "\(haveIce) ice cubes"
+        buyLemonLabel.text = "\(buyLemons)"
+        buyIceLabel.text = "\(buyIce)"
+        mixLemonLabel.text = "\(mixLemon)"
+        mixIceLabel.text = "\(mixIce)"
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,12 +60,52 @@ class ViewController: UIViewController {
     
     //BUY actions
     @IBAction func buyLessLemonsButtonPressed() {
+        if buyLemons > 0 {
+        buyLemons -= 1
+        haveLemons -= 1
+        haveCash += 2
+        
+        updateView()
+        }else{
+            if haveLemons > 0 {
+                buyLemons -= 1
+                haveLemons -= 1
+                haveCash += 2
+                updateView()
+            }
+        }
     }
     @IBAction func buyMoreLemonsButtonPressed() {
+        if haveCash > 1 {
+            buyLemons += 1
+            haveLemons += 1
+            haveCash -= 2
+            updateView()
+        }
     }
     @IBAction func buyLessIceButtonPressed() {
+        if buyIce > 0 {
+            buyIce -= 1
+            haveIce -= 1
+            haveCash += 1
+            
+            updateView()
+        }else{
+            if haveIce > 0 {
+                buyIce -= 1
+                haveIce -= 1
+                haveCash += 1
+                updateView()
+            }
+        }
     }
     @IBAction func buyMoreIceButtonPressed() {
+        if haveCash > 0 {
+            buyIce += 1
+            haveIce += 1
+            haveCash -= 1
+            updateView()
+        }
     }
     
     
@@ -61,10 +120,23 @@ class ViewController: UIViewController {
     }
     
     //Start day action
+    //no dejar que funcione si no tienes al menos un limón y un cubito de hielo
     @IBAction func startDayButtonPressed() {
     }
     
-
+    
+    //HELPER functions
+    func updateView(){
+        haveCashLabel.text = "$\(haveCash)"
+        haveLemonsLabel.text = "\(haveLemons) lemon"
+        haveIceLabel.text = "\(haveIce) ice cubes"
+        buyLemonLabel.text = "\(buyLemons)"
+        buyIceLabel.text = "\(buyIce)"
+        mixLemonLabel.text = "\(mixLemon)"
+        mixIceLabel.text = "\(mixIce)"
+    }
+    
+    
 
 }
 
