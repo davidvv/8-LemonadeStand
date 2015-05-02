167931f6af8203cca578dc393b238f4a2b5d211f
davidvv <idavidvv@gmail.com>
2015-05-03 00:20:52 +0200

    añadidos customer y Factory
    añadidos los archivos customer.swift y factory.swift

---
+++ LemonadeStand/Factory.swift
@@ -0,0 +1,15 @@
+//
+//  Factory.swift
+//  LemonadeStand
+//
+//  Created by David Vences Vaquero on 3/5/15.
+//  Copyright (c) 2015 David. All rights reserved.
+//
+
+import Foundation
+
+class Factory {
+    class func customerCreator {
+        //la idea es que esto cree un array de clientes. Lo ideal es que lo cree también dependiendo del tiempo, que sera una función random de 3 valores.
+    }
+}
